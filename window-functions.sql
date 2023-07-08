6c2157871e83639b72e03ab36588c62036a7234a
Julian Rivera <julianarevircoins@gmail.com>
2023-07-07 22:22:45 -0400

    order by

--- window-functions.sql
+++ window-functions.sql
@@ -7,7 +7,21 @@
 --     amount_tipped,
 --     SUM(amount_tipped) OVER()
 -- FROM bookings;
+-- SELECT booking_date,
+--     amount_tipped,
+--     SUM(amount_tipped) OVER(PARTITION BY booking_date)
+-- FROM bookings;
+-- SELECT booking_date,
+--     amount_tipped,
+--     SUM(amount_tipped) OVER(
+--         PARTITION BY booking_date
+--         ORDER BY amount_billed
+--     )
+-- FROM bookings;
 SELECT booking_date,
     amount_tipped,
-    SUM(amount_tipped) OVER(PARTITION BY booking_date)
+    RANK() OVER(
+        PARTITION BY booking_date
+        ORDER BY amount_tipped
+    )
 FROM bookings;
